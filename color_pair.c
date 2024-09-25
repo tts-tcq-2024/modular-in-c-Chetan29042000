67391e15ae9dd07d71f5a8296c606e768879d2a6
Chetan29042000 <chetansgowda29042000@gmail.com>
2024-09-25 14:39:18 +0530

    Update color_pair.c

--- color_pair.c
+++ color_pair.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <assert.h>
+#include "color_pair.h"
 
+const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
+const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+const int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s", MajorColorNames[colorPair->majorColor], MinorColorNames[colorPair->minorColor]);
+}
