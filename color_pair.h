cf524f30a3f5fa8d3e7ef32897326b01f22fde9e
Chetan29042000 <chetansgowda29042000@gmail.com>
2024-09-25 14:44:29 +0530

    Update color_pair.h

--- color_pair.h
+++ color_pair.h
@@ -5,14 +5,23 @@ enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
 enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
 
 typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
+    enum MajorColor majorColor;  
+    enum MinorColor minorColor;   
 } ColorPair;
 
+// Converts a ColorPair to a string representation.
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
+
+// Gets a ColorPair from a given pair number.
 ColorPair GetColorFromPairNumber(int pairNumber);
+
+// Gets the pair number from a ColorPair.
 int GetPairNumberFromColor(const ColorPair* colorPair);
+
+// Tests number to ColorPair conversion.
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+
+// Tests ColorPair to number conversion.
 void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
 
 #endif // COLOR_PAIR_H
