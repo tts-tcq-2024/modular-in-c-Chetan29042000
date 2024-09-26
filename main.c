bf5a80f9ddc33d9ea46776f89fc4191010403ff1
Chetan29042000 <chetansgowda29042000@gmail.com>
2024-09-26 11:46:40 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
-#include "color_pair.h"
+#include "test.c"
 
 int main() {
+    // Test cases for converting pair numbers to ColorPair
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
+
+    // Test cases for converting ColorPair to pair numbers
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
+
     return 0;
 }
