8899ae57a8be165ceb194bacf99756dd60fa705f
Chetan29042000 <chetansgowda29042000@gmail.com>
2024-09-26 11:45:33 +0530

    Update color_pair.h

--- color_pair.h
+++ color_pair.h
@@ -1,27 +1,25 @@
 #ifndef COLOR_PAIR_H
 #define COLOR_PAIR_H
 
+// Enumeration for major colors
 enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
+
+// Enumeration for minor colors
 enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
 
+// Struct representing a color pair
 typedef struct {
-    enum MajorColor majorColor;  
-    enum MinorColor minorColor;   
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
 } ColorPair;
 
-// Converts a ColorPair to a string representation.
+// Converts a ColorPair to a string representation
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 
-// Gets a ColorPair from a given pair number.
+// Gets a ColorPair from a given pair number
 ColorPair GetColorFromPairNumber(int pairNumber);
 
-// Gets the pair number from a ColorPair.
+// Gets the pair number from a ColorPair
 int GetPairNumberFromColor(const ColorPair* colorPair);
 
-// Tests number to ColorPair conversion.
-void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
-
-// Tests ColorPair to number conversion.
-void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
-
-#endif
+#endif // COLOR_PAIR_H
