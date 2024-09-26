088dc19ed11d8a3c741c29727bc551dfe65a75ec
Chetan29042000 <chetansgowda29042000@gmail.com>
2024-09-26 11:45:58 +0530

    Update color_pair.c

--- color_pair.c
+++ color_pair.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
-#include <assert.h>
 #include "color_pair.h"
 
+// Array for storing major color names
 const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
+
+// Array for storing minor color names
 const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+
+// Number of minor colors
 const int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-// Converts a ColorPair to a string representation.
+// Converts a ColorPair to a string representation
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s", MajorColorNames[colorPair->majorColor], MinorColorNames[colorPair->minorColor]);
 }
 
-// Gets a ColorPair from a given pair number.
+// Gets a ColorPair from a given pair number
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
@@ -20,7 +24,7 @@ ColorPair GetColorFromPairNumber(int pairNumber) {
     return colorPair;
 }
 
-// Gets the pair number from a ColorPair.
+// Gets the pair number from a ColorPair
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
 }
