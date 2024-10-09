dd346bc80b6b65b7187ee31104ac9515adc09c72
Chetan29042000 <chetansgowda29042000@gmail.com>
2024-10-09 10:25:04 +0530

    Update color_pair.c

--- color_pair.c
+++ color_pair.c
@@ -1,30 +1,37 @@
+#include "color.h"
 #include <stdio.h>
-#include "color_pair.h"
 
-// Array for storing major color names
-const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
-// Array for storing minor color names
-const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-// Number of minor colors
-const int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+const int MAX_COLORPAIR_NAME_CHARS = 16;
 
-// Converts a ColorPair to a string representation
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s", MajorColorNames[colorPair->majorColor], MinorColorNames[colorPair->minorColor]);
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
 }
 
-// Gets a ColorPair from a given pair number
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    colorPair.minorColor = (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
     return colorPair;
 }
 
-// Gets the pair number from a ColorPair
 int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
 }
