3b6593e165e5fd9b2e4444236c582c4ef7d435c7
Chetan29042000 <chetansgowda29042000@gmail.com>
2024-10-09 10:25:27 +0530

    Update color_pair.h

--- color_pair.h
+++ color_pair.h
@@ -1,19 +1,20 @@
-#ifndef COLOR_PAIR_H
-#define COLOR_PAIR_H
+#ifndef COLOR_H
+#define COLOR_H
 
-// Enumeration for major colors
 enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
-
-// Enumeration for minor colors
 enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
 
-// Struct representing a color pair
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+extern int numberOfMajorColors;
+extern int numberOfMinorColors;
+extern const int MAX_COLORPAIR_NAME_CHARS;
+
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
 
-// Function prototypes
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
