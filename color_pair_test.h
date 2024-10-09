bd0bef4dfab2e2a247fe7ebb8716390433758b9e
Chetan29042000 <chetansgowda29042000@gmail.com>
2024-10-09 10:24:18 +0530

    Create color_pair_test.h

---
+++ color_pair_test.h
@@ -0,0 +1,9 @@
+#ifndef TEST_COLOR_H
+#define TEST_COLOR_H
+
+#include "color.h"
+
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+
+#endif 
