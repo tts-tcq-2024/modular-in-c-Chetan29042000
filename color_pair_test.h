4861330e491736604ddd91618a07fc6b5b6a48bf
Chetan29042000 <chetansgowda29042000@gmail.com>
2024-10-09 10:27:55 +0530

    Update color_pair_test.h

--- color_pair_test.h
+++ color_pair_test.h
@@ -1,7 +1,7 @@
 #ifndef TEST_COLOR_H
 #define TEST_COLOR_H
 
-#include "color.h"
+#include "color_pair.h"
 
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
